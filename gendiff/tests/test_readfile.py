58e4cd29909da4575cd8ba5721552a527fa51414
devriez <a.moiseikin@gmail.com>
2021-01-13 15:42:02 +0300

    fix lint errors in test_readfile.py

--- gendiff/tests/test_readfile.py
+++ gendiff/tests/test_readfile.py
@@ -3,15 +3,15 @@ from gendiff.readfile import read_json_file
 
 def test_read_json_file():
     file1 = {
-    "host": "hexlet.io",
-    "timeout": 50,
-    "proxy": "123.234.53.22",
-    "follow": False
+      "host": "hexlet.io",
+      "timeout": 50,
+      "proxy": "123.234.53.22",
+      "follow": False
     }
     file2 = {
-    "timeout": 20,
-    "verbose": True,
-    "host": "hexlet.io"
+      "timeout": 20,
+      "verbose": True,
+      "host": "hexlet.io"
     }
 
     assert read_json_file('gendiff/tests/fixtures/file1.json') == file1
