e48ddc792e1163704d5bd98f3400a902b5a23516
devriez <a.moiseikin@gmail.com>
2021-01-13 15:53:41 +0300

    del fixtures for result

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,5 +1,6 @@
 from gendiff.scripts.generate_diff import generate_diff
 
+
 def test_generate_diff():
     given_result = '''{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22
   - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'''
