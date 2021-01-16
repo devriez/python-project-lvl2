2fb6295783c327d4a2bb2ec1a3f2b544d9ac423a
devriez <a.moiseikin@gmail.com>
2021-01-16 21:13:55 +0300

    make test for makediff_nested

--- gendiff/makediff.py
+++ gendiff/makediff.py
@@ -16,10 +16,9 @@ def make_diff(file1, file2):
             if file1[key] == file2[key]:
                 result[key] = {'status1': 'unchanged', 'body1': file1[key]}
             elif (type(file1[key]) != dict) or (type(file2[key]) != dict):
-                result[key] = {'status1': 'deleted', 'body1': file1[key],
-                               'status2': 'added', 'body2': file2[key]}
+                result[key] = {'status1': 'replaced', 'body1': file1[key], 'body2': file2[key]}
             else:
-                result[key] = {'status1': 'unchanged', 'body1': make_diff(file1[key], file2[key])}
+                result[key] = {'status1': 'changed', 'body1': make_diff(file1[key], file2[key])}
 
     return result
 
