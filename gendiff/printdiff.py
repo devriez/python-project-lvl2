3516d8716255c53b51c0cd628342d98a68f0439f
devriez <a.moiseikin@gmail.com>
2021-01-16 23:16:10 +0300

    write print diff

--- gendiff/printdiff.py
+++ gendiff/printdiff.py
@@ -1,3 +1,24 @@
-def print_diff(path_to_json):
-    print('Hello')
-    return 'string result'
+#def make_string(diff, result, sign, key, body)
+#    return result.append(f"  {sign} {key}: {diff[key]['body1']}")
+
+def print_diff(diff):
+    result = ['{']
+    sorted_keys = sorted(diff.keys())
+
+    for key in sorted_keys:
+        if diff[key]['status1'] == 'add':
+            #make_string(diff, result, "+", key, body)
+            result.append(f"  + {key}: {diff[key]['body1']}")
+        if diff[key]['status1'] == 'deleted':
+            result.append(f"  - {key}: {diff[key]['body1']}")
+        if diff[key]['status1'] == 'unchanged':
+            result.append(f"    {key}: {diff[key]['body1']}")
+        if diff[key]['status1'] == 'replaced':
+            result.append(f"  - {key}: {diff[key]['body1']}")
+            result.append(f"  + {key}: {diff[key]['body2']}")        
+        if diff[key]['status1'] == 'changed':
+            diff_nested = diff[key]['body1']
+            result.append(f"    {key}: {print_diff(diff_nested)}")
+
+    result.append('}')
+    return '\n'.join(result)
