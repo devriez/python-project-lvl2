36b284c3b53b96979809efbd85807fd277754189
devriez <a.moiseikin@gmail.com>
2021-01-17 19:08:13 +0300

    fixing printdiff

--- gendiff/printdiff.py
+++ gendiff/printdiff.py
@@ -1,18 +1,23 @@
 def print_diff(diff, indent=0):
+
+    if type(diff) != dict:
+        return diff
+
     step = ' ' * indent
+    next_indent = indent + 4
     result = ['{']
     sorted_keys = sorted(diff.keys())
 
     for key in sorted_keys:
         if diff[key]['status1'] == 'added':
-            result.append(f"{step}  + {key}: {diff[key]['body1']}")
+            result.append(f"{step}  + {key}: {print_diff(diff[key]['body1'], next_indent)}")
         if diff[key]['status1'] == 'deleted':
-            result.append(f"{step}  - {key}: {diff[key]['body1']}")
+            result.append(f"{step}  - {key}: {print_diff(diff[key]['body1'], next_indent)}")
         if diff[key]['status1'] == 'unchanged':
-            result.append(f"{step}    {key}: {diff[key]['body1']}")
+            result.append(f"{step}    {key}: {print_diff(diff[key]['body1'], next_indent)}")
         if diff[key]['status1'] == 'replaced':
-            result.append(f"{step}  - {key}: {diff[key]['body1']}")
-            result.append(f"{step}  + {key}: {diff[key]['body2']}")
+            result.append(f"{step}  - {key}: {print_diff(diff[key]['body1'], next_indent)}")
+            result.append(f"{step}  + {key}: {print_diff(diff[key]['body2'], next_indent)}")
         if diff[key]['status1'] == 'changed':
             next_indent = indent + 4
             diff_nested = diff[key]['body1']
