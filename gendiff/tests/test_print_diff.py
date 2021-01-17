59402e791972792fdafeb308d1d4ca3b0efdb3ec
devriez <a.moiseikin@gmail.com>
2021-01-17 19:12:51 +0300

    add test for print nested short

--- gendiff/tests/test_print_diff.py
+++ gendiff/tests/test_print_diff.py
@@ -5,6 +5,8 @@ PATH_TO_JSON_RESULT_FLAT = 'gendiff/tests/fixtures/result_makediff_flat.json'
 PATH_TO_STRING_RESULT_FLAT = 'gendiff/tests/fixtures/result_printdiff_flat.txt'
 PATH_TO_JSON_RESULT_NESTED = 'gendiff/tests/fixtures/result_makediff_nested.json'
 PATH_TO_STRING_RESULT_NESTED = 'gendiff/tests/fixtures/result_printdiff_nested.txt'
+PATH_TO_JSON_RESULT_NESTED_SHORT = 'gendiff/tests/fixtures/result_makediff_nested_short.json'
+PATH_TO_STRING_RESULT_NESTED_SHORT = 'gendiff/tests/fixtures/result_printdiff_nested_short.txt'
 
 
 def test_print_diff():
@@ -14,6 +16,13 @@ def test_print_diff():
     result_flat = print_diff(result_json_flat)
 
     assert result_flat == correct_result_flat
+
+    f = open(PATH_TO_STRING_RESULT_NESTED_SHORT)
+    correct_result_nested_short = f.read()
+    result_json_nested_short = read_file(PATH_TO_JSON_RESULT_NESTED_SHORT)
+    result_nested_short = print_diff(result_json_nested_short)
+
+    assert result_nested_short == correct_result_nested_short
 
     f = open(PATH_TO_STRING_RESULT_NESTED)
     correct_result_nested = f.read()
