160e34d736874b0af1b7d749ca831dce03d8ddda
devriez <a.moiseikin@gmail.com>
2021-01-17 17:33:18 +0300

    fix fixtures, open all tests for makediff

--- gendiff/tests/test_make_diff.py
+++ gendiff/tests/test_make_diff.py
@@ -27,19 +27,19 @@ def test_make_diff():
     correct_result_nested_short = read_file(PATH_TO_RESULT_NESTED_SHORT)
     assert result_nested_json_short == correct_result_nested_short
 
-    #nested1_json = read_file(PATH_TO_NESTED1_JSON)
-    #nested2_json = read_file(PATH_TO_NESTED2_JSON)
-    #result_nested_json = make_diff(nested1_json, nested2_json)
-    #correct_result_nested = read_file(PATH_TO_RESULT_NESTED)
-    #assert result_nested_json == correct_result_nested
+    nested1_json = read_file(PATH_TO_NESTED1_JSON)
+    nested2_json = read_file(PATH_TO_NESTED2_JSON)
+    result_nested_json = make_diff(nested1_json, nested2_json)
+    correct_result_nested = read_file(PATH_TO_RESULT_NESTED)
+    assert result_nested_json == correct_result_nested
 
-    # flat1_yml = read_file(PATH_TO_FLAT1_YML)
-    # flat2_yml = read_file(PATH_TO_FLAT2_YML)
-    # result_flat_yml = make_diff(flat1_yml, flat2_yml)
-    # assert result_flat_yml == read_file(PATH_TO_RESULT_FLAT)
+    flat1_yml = read_file(PATH_TO_FLAT1_YML)
+    flat2_yml = read_file(PATH_TO_FLAT2_YML)
+    result_flat_yml = make_diff(flat1_yml, flat2_yml)
+    assert result_flat_yml == read_file(PATH_TO_RESULT_FLAT)
 
-    # nested1_yml = read_file(PATH_TO_NESTED1_YML)
-    # nested2_yml = read_file(PATH_TO_NESTED2_YML)
-    # result_nested_yml = make_diff(nested1_yml, nested2_yml)
-    # correct_result_nested = read_file(PATH_TO_RESULT_NESTED)
-    # assert result_nested_yml == correct_result_nested
+    nested1_yml = read_file(PATH_TO_NESTED1_YML)
+    nested2_yml = read_file(PATH_TO_NESTED2_YML)
+    result_nested_yml = make_diff(nested1_yml, nested2_yml)
+    correct_result_nested = read_file(PATH_TO_RESULT_NESTED)
+    assert result_nested_yml == correct_result_nested
