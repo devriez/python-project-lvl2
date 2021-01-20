f861a18465d8ef65a901edba3d43734ce34e44d4
devriez <a.moiseikin@gmail.com>
2021-01-20 13:16:14 +0300

    plain work

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -4,8 +4,11 @@ def _make_value(value):
     if type(value) == bool:
         return str(value).lower()
     if value is None:
-        return 'null' 
+        return 'null'
+    if type(value) == str:
+        return f"'{value}'"
     return value
+
 
 def _make_string_from_template(path, diff, key):
     template = {
@@ -14,36 +17,33 @@ def _make_string_from_template(path, diff, key):
             f"{_make_value(diff[key]['body1'])}"
             ),
         'deleted': f"Property '{path}' was removed",
-        'removed': (
+        'replaced': (
             f"Property '{path}' was updated. "
-            f"From '{_make_value(diff[key]['body1'])}' "
-            f"to '{_make_value(diff[key]['body2'])}'"
+            f"From {_make_value(diff[key]['body1'])} "
+            f"to {_make_value(diff[key].get('body2'))}"
             )
     }
     status = diff[key]['status1']
     return template[status]
 
-def _make_string(diff, path):
+
+def plain(diff, path=''):
     keys = sorted(diff.keys())
+    result = []
 
     for key in keys:
-        path = path + '.' + key
+        path_to_value = path + key
         if diff[key]['status1'] == 'added':
-            return _make_string_from_template(path, diff, key)
-        if diff[key]['status1'] == 'deleted':
-            return _make_string_from_template(path, diff, key)
-        if diff[key]['status1'] == 'removed':
-            return _make_string_from_template(path, diff, key)
-        if diff[key]['status1'] == 'changed':
-            return _make_string(diff[key]['body1'], path) 
-
-            
-def plain(diff):
-    result=[]
-    keys = sorted(diff.keys())
-
-    for key in keys:
-        string = _make_string(diff[key]['body1'], key)
+            string = _make_string_from_template(path_to_value, diff, key)
+        elif diff[key]['status1'] == 'deleted':
+            string = _make_string_from_template(path_to_value, diff, key)
+        elif diff[key]['status1'] == 'replaced':
+            string = _make_string_from_template(path_to_value, diff, key)
+        elif diff[key]['status1'] == 'changed':
+            next_step_path = path_to_value + '.'
+            string = plain(diff[key]['body1'], next_step_path)
+        else:
+             continue
         result.append(string)
 
-    return '/n'.join(result)    
+    return '\n'.join(result)
